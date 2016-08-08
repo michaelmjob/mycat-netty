57cba0418613e92e69e03dd076d284fe8e5ac947
jianhai.xu <jianhai.xu@ele.me>
2016-08-08 22:15:08 +0800

    add perfect db login  test

--- src/test/java/io/mycat/netty/mysql/protocolTest.java
+++ src/test/java/io/mycat/netty/mysql/protocolTest.java
@@ -6,49 +6,54 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.sun.source.tree.AssertTree;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * Created by snow_young on 16/8/5.
  */
 public class protocolTest {
-//    private String dbUrl = "jdbc:mysql://localhost:8090/schema_main?connectTimeout=1000";
-//    private String dbUrl = "jdbc:mysql://localhost:6100/schema_main?connectTimeout=1000";
+
+//    @Rule
+//    public ExpectedException thrown= ExpectedException.none();
+
 
 //    @Test
-    public void loginWithoutPass() throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        String dbUrl = "jdbc:mysql://localhost:8090/schema_main?connectTimeout=1000";
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("找不到驱动程序类 ，加载驱动失败！");
-            e.printStackTrace();
-        }
-
-        String username = "root";
-        String password = "root";
-        // test success
-        Connection con = null;
-        try {
-            con = DriverManager.getConnection(dbUrl, username, password);
-        } catch (SQLException se) {
-            System.out.println("数据库连接失败！");
-            se.printStackTrace();
-            Assert.assertTrue(false);
-        } finally {
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+//    public void loginWithoutPass() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+//        String dbUrl = "jdbc:mysql://localhost:8090/schema_main?connectTimeout=1000";
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("找不到驱动程序类 ，加载驱动失败！");
+//            e.printStackTrace();
+//        }
+//
+//        String username = "root";
+//        String password = "root";
+//        // test success
+//        Connection con = null;
+//        try {
+//            con = DriverManager.getConnection(dbUrl, username, password);
+//        } catch (SQLException se) {
+//            System.out.println("数据库连接失败！");
+//            se.printStackTrace();
+//            Assert.assertTrue(false);
+//        } finally {
+//            if (con != null) {
+//                try {
+//                    con.close();
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
 
     @Test
-    public void loginWithPass(){
+    public void testLoginWithPassSucceess() {
         try {
             //加载MySql的驱动类
             Class.forName("com.mysql.jdbc.Driver");
@@ -78,10 +83,28 @@ public class protocolTest {
                 }
             }
         }
+    }
 
-        String wrongpass = "zhangsan";
+    @Test
+    public void testLoginWithPassWrongPass(){
         try {
+            //加载MySql的驱动类
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("找不到驱动程序类 ，加载驱动失败！");
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }
+
+        String username = "xujianhai";
+        String wrongpass = "zhangsan";
+        String dbUrl = "jdbc:mysql://localhost:8090/schema?connectTimeout=1000";
+        Connection con = null;
+        try {
+//            thrown.expect(SQLException.class);
+//            thrown.expectMessage("Access denied for user 'xujianhai'");
             con = DriverManager.getConnection(dbUrl, username, wrongpass);
+            Assert.assertTrue(false);
         } catch (SQLException se) {
             System.out.println("数据库连接失败！");
             se.printStackTrace();
@@ -94,9 +117,75 @@ public class protocolTest {
                 }
             }
         }
-
-
-
-
     }
+
+    @Test
+    public void testLoginWithPassWrongDB(){
+        try {
+            //加载MySql的驱动类
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("找不到驱动程序类 ，加载驱动失败！");
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }
+
+        String username = "xujianhai";
+        String wrongpass = "zhangsan";
+        String dbUrl = "jdbc:mysql://localhost:8090/schemawrong?connectTimeout=1000";
+        Connection con = null;
+        try {
+//            thrown.expect(SQLException.class);
+//            thrown.expectMessage("Access denied for user 'xujianhai'");
+            con = DriverManager.getConnection(dbUrl, username, wrongpass);
+            Assert.assertTrue(false);
+        } catch (SQLException se) {
+            System.out.println("数据库连接失败！");
+            se.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testLoginWithPassWrongUser(){
+        try {
+            //加载MySql的驱动类
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("找不到驱动程序类 ，加载驱动失败！");
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }
+
+        String username = "xujianhaiwronguser";
+        String wrongpass = "zhangsan";
+        String dbUrl = "jdbc:mysql://localhost:8090/schema?connectTimeout=1000";
+        Connection con = null;
+        try {
+//            thrown.expect(SQLException.class);
+//            thrown.expectMessage("Access denied for user 'xujianhai'");
+            con = DriverManager.getConnection(dbUrl, username, wrongpass);
+            Assert.assertTrue(false);
+        } catch (SQLException se) {
+            System.out.println("数据库连接失败！");
+            se.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 }
